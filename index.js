920164a2eebc793d87673f25aaf0a9b8ddbdd100
Ryan Erricson <ryanerricson@gmail.com>
2021-05-03 14:34:40 -0400

    commit tree ddef4aecbee2f9c2ec662b1ca8a5c24fd1b959a8

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names) {
+    const newArray = []
+    for (let n = 0; n < names.length; n++ ) {
+        newArray.push(`Thank you, ${names[n]}, for the wonderful surprise gift!`)
+    }
+    return newArray
+}
+
+function countDown(a) {
+ while (a >= 0){
+     console.log(a--);
+ }
+}
